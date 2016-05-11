6dded5ccfa5379e79650addd50170f17e974aa54
dohantaur <dohant.aurelien@gmail.com>
2016-05-11 10:48:28 +0200

    fix foreign key migration

--- db/migrate/20160510094837_add_user_to_doorkeeper.rb
+++ db/migrate/20160510094837_add_user_to_doorkeeper.rb
@@ -1,6 +1,6 @@
 class AddUserToDoorkeeper < ActiveRecord::Migration
   def change
-    add_foreign_key :oauth_access_grants, :user, column: :resource_owner_id
-    add_foreign_key :oauth_access_tokens, :user, column: :resource_owner_id
+    add_foreign_key :oauth_access_grants, :users, column: :resource_owner_id
+    add_foreign_key :oauth_access_tokens, :users, column: :resource_owner_id
   end
 end
