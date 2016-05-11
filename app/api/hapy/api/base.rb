238ce7fbd8e0d7025ab04367a473092bfd68826d
dohantaur <dohant.aurelien@gmail.com>
2016-05-11 10:04:22 +0200

    plop

--- app/api/hapy/api/base.rb
+++ app/api/hapy/api/base.rb
@@ -3,8 +3,6 @@ module Hapy::API
     format :json
     content_type :json, 'application/json; charset=utf-8'
 
-    helpers { def method; options[:method][0]; end }
-
     mount Hapy::API::V1::Base
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  use_doorkeeper
+  use_doorkeeper do
+    skip_controllers :authorizations, :applications, :authorized_applications
+  end
   mount Hapy::API::Base => '/'
 end
