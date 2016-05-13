099c0435230f7364fa8788e656078b7e1132619d
dohantaur <dohant.aurelien@gmail.com>
2016-05-13 10:20:49 +0200

    add seeds

--- db/migrate/20160511132141_create_plants.rb
+++ db/migrate/20160511132141_create_plants.rb
@@ -5,8 +5,8 @@ class CreatePlants < ActiveRecord::Migration
       t.string :description
       t.datetime :plantationDate
       t.string :picture_url
-      t.references :green_houses, index: true
-      t.references :plant_templates
+      t.references :green_house, index: true
+      t.references :plant_template
       t.timestamps null: false
     end
   end
