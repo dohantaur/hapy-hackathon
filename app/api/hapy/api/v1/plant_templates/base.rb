ed1b3f3432572a45ff3920e4d18fe50c8443f95c
dohantaur <dohant.aurelien@gmail.com>
2016-06-15 15:45:42 +0200

    plop

--- app/api/hapy/api/v1/plant_templates/base.rb
+++ app/api/hapy/api/v1/plant_templates/base.rb
@@ -5,8 +5,16 @@ module Hapy::API::V1::PlantTemplates
       doorkeeper_authorize!
     end
 
+    params do
+      optional :name, type: String
+    end
     get do
-      present :data, PlantTemplate.all, with: Hapy::Entities::PlantTemplateEntity
+      puts params[:name]
+      if params[:name].blank?
+        present :data, PlantTemplate.all, with: Hapy::Entities::PlantTemplateEntity
+      else
+        present :data, PlantTemplate.where('name LIKE ?', "%#{params[:name]}%"), with: Hapy::Entities::PlantTemplateEntity
+      end
     end
 
   end
